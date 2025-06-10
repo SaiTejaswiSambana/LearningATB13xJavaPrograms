b36b03c1e488036c800ef01e1ab4136cfddabfb6
SaiTejaswiSambana <saitejaswisambana@gmail.com>
2025-06-10 13:51:43 +0530

    June 10th -Printing 9th tabke using printf()

---
+++ src/ex_01_Java_Basic/Lab004_tableof9.java
@@ -0,0 +1,49 @@
+package ex_01_Java_Basic;
+
+public class Lab004_tableof9 {
+    public static void main(String[] args) {
+
+            int a=9;
+            int b=1;
+            System.out.printf("%d X %d = %d",a,b,a*b);
+            System.out.println();
+            b++;
+
+            System.out.printf("%d X %d = %d",a,b,a*b);
+            System.out.println();
+            b++;
+
+            System.out.printf("%d X %d = %d",a,b,a*b);
+            System.out.println();
+            b++;
+
+            System.out.printf("%d X %d = %d",a,b,a*b);
+            System.out.println();
+            b++;
+
+            System.out.printf("%d X %d = %d",a,b,a*b);
+            System.out.println();
+            b++;
+
+            System.out.printf("%d X %d = %d",a,b,a*b);
+            System.out.println();
+            b++;
+
+            System.out.printf("%d X %d = %d",a,b,a*b);
+            System.out.println();
+            b++;
+
+            System.out.printf("%d X %d = %d",a,b,a*b);
+            System.out.println();
+            b++;
+
+            System.out.printf("%d X %d = %d",a,b,a*b);
+            System.out.println();
+            b++;
+
+            System.out.printf("%d X %d = %d",a,b,a*b);
+            System.out.println();
+
+        }
+    }
+
