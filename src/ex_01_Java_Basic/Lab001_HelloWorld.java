51bd93051707766c73b668782c5d87ce02d91590
SaiTejaswiSambana <SaitejaswiSambana@gmail.com>
2025-06-10 13:55:22 +0530

    Add files via upload

---
+++ src/ex_01_Java_Basic/Lab001_HelloWorld.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+import com.sun.tools.javac.Main;
+
+public class Lab001_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
