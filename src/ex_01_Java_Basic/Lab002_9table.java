098c6b464e4ca80d32a4dfda9cbc9aca77651b29
SaiTejaswiSambana <saitejaswisambana@gmail.com>
2025-06-12 15:57:02 +0530

    June 11th examples of Arthimetic,Relational and Logical operators

---
+++ src/ex_01_Java_Basic/Lab002_9table.java
@@ -0,0 +1,47 @@
+package ex_01_Java_Basic;
+
+public class Lab002_9table {
+    public static void main(String[] args) {
+        int a=9;
+        int b=1;
+        System.out.printf("%d X %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d X %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d X %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d X %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d X %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d X %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d X %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d X %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d X %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d X %d = %d",a,b,a*b);
+        System.out.println();
+
+    }
+}
