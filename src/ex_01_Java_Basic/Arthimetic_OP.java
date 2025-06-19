7b591be28445dac16d8ec017dac72dd2239046be
SaiTejaswiSambana <SaitejaswiSambana@gmail.com>
2025-06-19 08:11:41 +0530

    Add files via upload

---
+++ src/ex_01_Java_Basic/Arthimetic_OP.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class Arthimetic_OP {
+    public static void main(String[] args) {
+        int a=20;
+        int b=10;
+        System.out.println(a+b);
+        System.out.println( a-b);
+        System.out.println("Multiplication "+ a*b);
+        System.out.println("Division "+ a/b);
+        System.out.println("Modulous "+ a%b);
+    }
+}
