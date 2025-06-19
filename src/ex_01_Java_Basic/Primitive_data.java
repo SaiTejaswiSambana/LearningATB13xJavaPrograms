20fc1fb66e5d11faa4e260bd40af4c850a6781f8
SaiTejaswiSambana <SaitejaswiSambana@gmail.com>
2025-06-19 08:05:33 +0530

    Add files via upload

---
+++ src/ex_01_Java_Basic/Primitive_data.java
@@ -0,0 +1,15 @@
+package Test;
+
+public class Primitive_data {
+    public static void main(String[] args) {
+        int a=25;
+        boolean b= 10>20;
+        double d=12.3;
+        char c='A';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
