70b3bb8b968de4d148e94aa91f87ea9d24edf26d
SaiTejaswiSambana <saitejaswisambana@gmail.com>
2025-06-26 14:47:41 +0530

    Palindrome -June 25th commit for 23rd june task

---
+++ src/String_Java_Exam/BasicString_Functions_1.java
@@ -0,0 +1,12 @@
+package String_Java_Exam;
+
+public class BasicString_Functions_1 {
+    public static void main(String[] args) {
+        //Write a Java program that demonstrates basic string operations including concatenation, length, substring, and character extraction.
+        String input="Hello World";
+        System.out.println("LENGTH :" + " " +input.length());
+        System.out.println("First char: "+ input.charAt(0));
+        System.out.println("Substring: "+input.substring(6,11));
+
+    }
+}
