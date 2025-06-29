876262c4bc98aebc254c7416fb10ab2b19a454e3
SaiTejaswiSambana <saitejaswisambana@gmail.com>
2025-06-29 09:06:38 +0530

    29th changes in increment and decrement quest

--- src/ATB_XM_Test_Programs/Increment_Diff.java
+++ src/ATB_XM_Test_Programs/Increment_Diff.java
@@ -2,13 +2,18 @@ package ATB_XM_Test_Programs;
 
 public class Increment_Diff {
     public static void main(String[] args) {
-        int i=10;
-        System.out.println("++i is"+ ++i);
+        int i=5;
+        System.out.println("++i is "+ ++i);
         System.out.println("i value is " + i);
-
-        int a_post= 10;
+        System.out.println("--i is "+ --i);
+        System.out.println("i value is " + i);
+        int a_post= 5;
         System.out.println("a_post++ is "+ a_post++);
         System.out.println("a_post value is "+ a_post);
+        System.out.println("a_post++ is "+ a_post--);
+        System.out.println("a_post++ is "+ a_post);
+
+
 
         }
 }
