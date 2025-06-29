732a5fe21796fb61a35e938bcf3b2748e11ead18
SaiTejaswiSambana <saitejaswisambana@gmail.com>
2025-06-29 09:14:57 +0530

    29th changes in for loop quest

--- src/ex_02_userInput/ForLoop_1to10_XM.java
+++ src/ex_02_userInput/ForLoop_1to10_XM.java
@@ -1,8 +1,12 @@
 package ex_02_userInput;
 
+import java.util.Scanner;
+
 public class ForLoop_1to10_XM {
     public static void main(String[] args) {
-        for (int i=1;i<=10;i++){
+        Scanner scanner = new Scanner(System.in);
+        int num= scanner.nextInt();
+        for (int i=1;i<=num;i++){
             System.out.println(i);
         }
     }
